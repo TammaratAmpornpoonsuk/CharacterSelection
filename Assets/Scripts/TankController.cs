700081b421e6280cb02f00ca63b2aa85b2f8a39d
Tammarat Ampornpoonsuk <tammarat.amp@corepolygon.com>
2025-06-14 13:46:01 +0700

    Moving and Rotating Tank

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -5,14 +5,27 @@ public class TankController
     private TankModel _tankModel;
     private TankView _tankView;
 
+    private Rigidbody _rb;
+
     public TankController(TankModel tankModel, TankView tankView)
     {
         _tankModel = tankModel;
-        _tankView = tankView;
+        _tankView = GameObject.Instantiate<TankView>(tankView);
+        _rb = _tankView.GetRigidbody();
 
         _tankModel.SetTankController(this);
         _tankView.SetTankController(this);
+    }
 
-        GameObject.Instantiate(tankView.gameObject);
+    public void Move(float movement, float movementSpeed)
+    {
+        _rb.velocity = _tankView.transform.forward * movement * movementSpeed;
+    }
+
+    public void Rotate(float rotate, float rotateSpeed)
+    {
+        Vector3 vector = new Vector3(0f, rotate * rotateSpeed, 0f);
+        Quaternion deltaRotation = Quaternion.Euler(vector * Time.deltaTime);
+        _rb.MoveRotation(_rb.rotation * deltaRotation);
     }
 }
--- Assets/Scripts/TankView.cs
+++ Assets/Scripts/TankView.cs
@@ -4,8 +4,30 @@ public class TankView : MonoBehaviour
 {
     private TankController _tankController;
 
+    private float _movement;
+    private float _rotation;
+
+    public Rigidbody rb;
+
     public void SetTankController(TankController tankController)
     {
         _tankController = tankController;
+    }
+
+    public Rigidbody GetRigidbody() => rb;
+
+    private void Update()
+    {
+        Movement();
+        if (_movement != 0)
+            _tankController.Move(_movement, 30);
+        if (_rotation != 0)
+            _tankController.Rotate(_rotation, 150);
+    }
+
+    private void Movement()
+    {
+        _movement = Input.GetAxis("Vertical");
+        _rotation = Input.GetAxis("Horizontal");
     }
 }
